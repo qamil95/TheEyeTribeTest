b34235dae239b3e30842c184518ac0760047762f
Kamil Ziętek (kamizie584) <kamizie584@student.polsl.pl>
2017-06-19 17:23:48 +0200

    point and button markers

--- TheEyeTribeTest/Form1.Designer.cs
+++ TheEyeTribeTest/Form1.Designer.cs
@@ -28,21 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelX = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelY = new System.Windows.Forms.ToolStripStatusLabel();
+            this.radioButtonPosition = new System.Windows.Forms.RadioButton();
+            this.buttonPosition = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelX,
+            this.toolStripStatusLabelY});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 420);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(624, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelX
+            // 
+            this.toolStripStatusLabelX.Name = "toolStripStatusLabelX";
+            this.toolStripStatusLabelX.Size = new System.Drawing.Size(119, 17);
+            this.toolStripStatusLabelX.Text = "toolStripStatusLabelX";
+            // 
+            // toolStripStatusLabelY
+            // 
+            this.toolStripStatusLabelY.Name = "toolStripStatusLabelY";
+            this.toolStripStatusLabelY.Size = new System.Drawing.Size(119, 17);
+            this.toolStripStatusLabelY.Text = "toolStripStatusLabelY";
+            // 
+            // radioButtonPosition
+            // 
+            this.radioButtonPosition.AutoSize = true;
+            this.radioButtonPosition.Location = new System.Drawing.Point(218, 64);
+            this.radioButtonPosition.Name = "radioButtonPosition";
+            this.radioButtonPosition.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonPosition.TabIndex = 3;
+            this.radioButtonPosition.TabStop = true;
+            this.radioButtonPosition.UseVisualStyleBackColor = true;
+            // 
+            // buttonPosition
+            // 
+            this.buttonPosition.Location = new System.Drawing.Point(12, 12);
+            this.buttonPosition.Name = "buttonPosition";
+            this.buttonPosition.Size = new System.Drawing.Size(25, 100);
+            this.buttonPosition.TabIndex = 4;
+            this.buttonPosition.Text = "button1";
+            this.buttonPosition.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.buttonPosition);
+            this.Controls.Add(this.radioButtonPosition);
+            this.Controls.Add(this.statusStrip1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelX;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelY;
+        private System.Windows.Forms.RadioButton radioButtonPosition;
+        private System.Windows.Forms.Button buttonPosition;
     }
 }
 
---
+++ TheEyeTribeTest/Point.cs
@@ -0,0 +1,89 @@
+﻿using EyeTribe.ClientSdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EyeTribe.ClientSdk.Data;
+
+namespace TheEyeTribeTest
+{
+    class Point : IGazeListener
+    {
+        double gX, gY, percentX, percentY;        
+        public Point()
+        {
+            // Connect client 
+            GazeManager.Instance.Activate(GazeManager.ApiVersion.VERSION_1_0);
+
+            // Register this class for events 
+            GazeManager.Instance.AddGazeListener(this);            
+        }
+
+        public double GX
+        {
+            get
+            {
+                return gX;
+            }
+
+            set
+            {
+                gX = value;
+            }
+        }
+
+        public double GY
+        {
+            get
+            {
+                return gY;
+            }
+
+            set
+            {
+                gY = value;
+            }
+        }
+
+        public double PercentX
+        {
+            get
+            {
+                return percentX;
+            }
+
+            set
+            {
+                percentX = value;
+            }
+        }
+
+        public double PercentY
+        {
+            get
+            {
+                return percentY;
+            }
+
+            set
+            {
+                percentY = value;
+            }
+        }
+
+        public void OnGazeUpdate(GazeData gazeData)
+        {
+            gX = gazeData.SmoothedCoordinates.X;
+            percentX = gX / GazeManager.Instance.ScreenResolutionWidth;
+            gY = gazeData.SmoothedCoordinates.Y;
+            percentY = gY / GazeManager.Instance.ScreenResolutionHeight;
+        }
+
+        public void Disconnect()
+        {
+            // Disconnect client
+            GazeManager.Instance.Deactivate();
+        }
+    }
+}
