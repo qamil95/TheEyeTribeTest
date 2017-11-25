3e6dcefb7ede93c89b6d8823a3c04a942301b647
Kamil Ziętek (kamizie584) <kamizie584@student.polsl.pl>
2017-11-25 17:07:42 +0100

    created server and client empy projects

---
+++ PONG Client/ClientWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace PONG_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BinaryReader reader;
+        private BinaryWriter writer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var client = new TcpClient("localhost", 8888);
+            var stream = client.GetStream();
+            reader = new BinaryReader(stream);
+            writer = new BinaryWriter(stream);
+        }
+    }
+}
---
+++ PONG Server/ServerWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace PONG_Server
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BinaryReader reader;
+        private BinaryWriter writer;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var listener = new TcpListener(IPAddress.Any, 8888);
+            listener.Start();
+            var client = listener.AcceptTcpClient();
+            var stream = client.GetStream();
+            reader = new BinaryReader(stream);
+            writer = new BinaryWriter(stream);
+        }
+    }
+}
